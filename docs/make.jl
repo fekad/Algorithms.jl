d412fd2b2f431761e6a5839ff6046117c3501183
Adam Fekete <adam.fekete@kcl.ac.uk>
2021-05-14 17:16:26 +0200

    deploy

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,9 @@ using Documenter, Algorithms
 
 makedocs(
     sitename="Algorithms",
+    # authors="Adam Fekete",
+    repo="https://github.com/fekad/Algorithms.jl/blob/{commit}{path}#L{line}",
+    format=Documenter.HTML(),
     pages = [
         "index.md",
         "Visualisation" => [
@@ -9,4 +12,10 @@ makedocs(
         ],
         "cheatsheet.md"
     ]
+)
+
+deploydocs(;
+    repo="github.com/fekad/Algorithms.jl",
+    branch = "gh-pages",
+    devbranch = "main",
 )
